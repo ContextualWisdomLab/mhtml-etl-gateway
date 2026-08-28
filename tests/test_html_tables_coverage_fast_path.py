edbeebad360f4d0dd3ce26933be21c398c4aeb2d
Seongho Bae <me@seonghobae.me>
2026-08-28 12:26:46 -0700

    test: remove unused fast-path import

--- tests/test_html_tables_coverage_fast_path.py
+++ tests/test_html_tables_coverage_fast_path.py
@@ -1,8 +1,9 @@
-import pytest
 from mhtml_etl_gateway.html_tables import _normalize_text
+
 
 def test_normalize_empty_list():
     assert _normalize_text([]) == ""
+
 
 def test_normalize_single_whitespace_only():
     assert _normalize_text(["   \n\t  "]) == ""
